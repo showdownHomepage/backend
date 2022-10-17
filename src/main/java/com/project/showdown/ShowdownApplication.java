eafa92c281eedd54a2b1401766913c540b5f1d69
Yun-Jehyeok <dbswpgur2@naver.com>
2022-10-17 10:48:06 +0900

    Settings

---
+++ src/main/java/com/project/showdown/ShowdownApplication.java
@@ -0,0 +1,13 @@
+package com.project.showdown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowdownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowdownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/showdown/ShowdownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.showdown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowdownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
