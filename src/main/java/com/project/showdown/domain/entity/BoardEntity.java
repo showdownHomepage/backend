65c8606c5e89c97aa278fb3273fb1c5911a5990c
Seulg2027 <seulgi2027@dgu.ac.kr>
2022-10-19 00:07:32 +0900

    feat: boardEntity 추가, repository 기본

--- src/main/java/com/project/showdown/domain/entity/BoardEntity.java
+++ src/main/java/com/project/showdown/domain/entity/BoardEntity.java
@@ -1,15 +1,26 @@
 package com.project.showdown.domain.entity;
 
+import com.sun.istack.NotNull;
 import lombok.Getter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Getter
 @Entity
 public class BoardEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private String create;
+
+    private String contents;
+
+    private Date date;
 }
--- src/main/java/com/project/showdown/domain/repository/BoardRepository.java
+++ src/main/java/com/project/showdown/domain/repository/BoardRepository.java
@@ -3,5 +3,11 @@ package com.project.showdown.domain.repository;
 import com.project.showdown.domain.entity.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class BoardRepository extends JpaRepository<BoardEntity, Long> {
+import java.util.List;
+
+public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
+
+    BoardEntity insertBoard(BoardEntity boardEntity);
+
+    List<BoardEntity> getBoardList();
 }
